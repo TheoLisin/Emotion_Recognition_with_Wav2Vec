101e79e6f00b8ce6c53139745f019b2122781782
TheodorusF <aldehydrcho@gmail.com>
2021-07-15 12:02:12 +0300

    del shit

--- audio_parser.py
+++ audio_parser.py
@@ -6,7 +6,6 @@ def save_wav(name, new_sr=16000):
     ogg_name = name + ".ogg"
     wav_name = name + ".wav"
     subprocess.run(['ffmpeg', "-i", ogg_name, wav_name])
-    dsac
     os.remove(ogg_name)
 
 
