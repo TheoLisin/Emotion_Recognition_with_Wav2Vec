ac952007a7b6296e7df418a25b47cdc24d7d55d3
TheoLisin <87002239+TheoLisin@users.noreply.github.com>
2021-09-26 03:19:27 +0300

    Update classification head

--- src/emo_recognition_bot/model_class.py
+++ src/emo_recognition_bot/model_class.py
@@ -22,18 +22,37 @@ class SpeechClassifierOutput(ModelOutput):
 class Wav2Vec2ClassificationHead(nn.Module):
     """Head for wav2vec classification task."""
 
+    # def __init__(self, config):
+    #     super().__init__()
+    #     self.dense = nn.Linear(config.hidden_size, config.hidden_size // 2)
+    #     self.dropout = nn.Dropout(config.final_dropout)
+    #     self.out_proj = nn.Linear(config.hidden_size // 2, config.num_labels)
+
+    # def forward(self, features, **kwargs):
+    #     x = features
+    #     x = self.dropout(x)
+    #     x = self.dense(x)
+    #     x = torch.tanh(x)
+    #     x = self.dropout(x)
+    #     x = self.out_proj(x)
+    #     return x
+
     def __init__(self, config):
         super().__init__()
-        self.dense = nn.Linear(config.hidden_size, config.hidden_size)
-        self.dropout = nn.Dropout(config.final_dropout)
-        self.out_proj = nn.Linear(config.hidden_size, config.num_labels)
+        self.dense1 = nn.Linear(config.hidden_size, config.hidden_size // 2)
+        self.dropout1 = nn.Dropout(.25)
+        self.dense2 = nn.Linear(config.hidden_size // 2, config.hidden_size // 4)
+        self.dropout2 = nn.Dropout(.25)
+        self.out_proj = nn.Linear(config.hidden_size // 4, config.num_labels)
 
     def forward(self, features, **kwargs):
         x = features
-        x = self.dropout(x)
-        x = self.dense(x)
-        x = torch.tanh(x)
-        x = self.dropout(x)
+        x = self.dropout1(x)
+        x = self.dense1(x)
+        x = torch.relu(x)
+        x = self.dropout2(x)
+        x = self.dense2(x)
+        x = torch.relu(x)
         x = self.out_proj(x)
         return x
 
